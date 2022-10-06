9089edda2819e32d260f87e72ffc3e03209e6115
Abdullah Fahad <78935603+abdfahad@users.noreply.github.com>
2022-10-06 17:03:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded',()=>{
                 alert(`${toDb.status} : something went wrong`);
             }
         }  
-        toDb.open('POST', 'http://localhost:3000/messages', true);
+        toDb.open('POST', 'https://arcane-woodland-17272.herokuapp.com/messages', true);
         toDb.setRequestHeader('Content-type', 'application/json');
         toDb.send(JSON.stringify(msg));
     })
